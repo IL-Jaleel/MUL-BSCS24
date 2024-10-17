cc86e80390214230c60812da850d113ce627f732
IL-Jaleel <jaleel.il@outlook.com>
2024-10-17 14:28:46 +0500

    Add files via upload
    Create DB /

---
+++ 17OCT24.sql
@@ -0,0 +1,13 @@
+create database NEW_STUDENT;
+
+use NEW_STUDENT;
+
+create table Student(
+ID int,
+[NAME] varchar(200),
+[Phone number] varchar(200)
+);
+
+
+ALTER  table Student
+ADD [FATHER NAME] varchar (200);
